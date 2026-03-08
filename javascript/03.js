e15300d60baa9a7b58bab8cbb9b916f1d7b8aee2
nari <naritaka329@gmail.com>
2026-03-08 13:41:42 +0900

    add js01

---
+++ javascript/03.js
@@ -0,0 +1,25 @@
+const N = Number(window.prompt("自然数を入力してください"));
+
+/* 1からNまでの「偶数の和」(evenSum) と 
+  「奇数の和」(oddSum) を計算します。 */
+
+/* ループを作成し、順番iが偶数なら(i%2 === 0　)ならevenSumにiを加えるなどします。 */
+
+let evenSum = 0
+
+let oddSum = 0
+
+for (let i = 0; i <= N; i++){
+  if (i % 2 == 0)
+    evenSum = evenSum + i
+  if (i % 2 == 1)
+    oddSum = oddSum + i
+}
+
+/* ２つの差分を表示
+  evenSumとoddSumの差分の絶対値を出力します。絶対値はMath.abs()を使います。
+  出力にはdocument.write()を使いましょう。
+*/
+
+let gap = Math.abs(evenSum - oddSum)
+console.log(gap)
