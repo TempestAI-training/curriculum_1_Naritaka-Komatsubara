52b2b8b716ae401d8d97fe0556a2d33e2b67d663
nari <naritaka329@gmail.com>
2026-03-20 17:19:34 +0900

    add final-frontend

---
+++ my-chatbot/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import Chat1 from "./Chat1";
+import Chat2 from "./Chat2";
+
+function App() {
+  const [site, setSite] = useState(true);
+
+  return (
+    <div className="App">
+
+       {site ? (
+              <header className="Box">
+        <p>Politics Study</p>
+          <h2>早苗さんの政策について学ぼう！</h2>
+          <h3>任意のお問い合わせに対して早苗さんの考えを返信します。</h3>
+          <div className="butt" onClick={() => setSite(!site)}>
+              チャットを始める
+          </div>
+
+         
+      </header>
+       ) : (<Chat2/>)}
+
+    </div>
+  );
+};
+
+export default App;
