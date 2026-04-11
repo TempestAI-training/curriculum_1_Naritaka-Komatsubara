57671401277bba7d90f0f5fab9dac03483d0b8ad
nari <naritaka329@gmail.com>
2026-04-11 16:49:33 +0900

    revise main.py

--- final-backend/backend/main.py
+++ final-backend/backend/main.py
@@ -14,6 +14,18 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "https://curriculum-1-naritaka-komatsubara-prpkeodgm-naritaka.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #CORS
 app.add_middleware(
     CORSMiddleware,
