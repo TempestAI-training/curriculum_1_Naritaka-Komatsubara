bcc5309d589c46e2a2f085d1f3a3bb13ecc1255b
nari <naritaka329@gmail.com>
2026-04-13 23:02:14 +0900

    revise connect.py

--- final-backend/backend/connect.py
+++ final-backend/backend/connect.py
@@ -2,13 +2,12 @@ import os
 import psycopg
 from dotenv import load_dotenv
 
-load_dotenv()
-
 def get_connection():
-    database_url = os.getenv("DATABASE_URL")
-    print("DATABASE_URL =", database_url)
-
-    if not database_url:
-        raise ValueError("DATABASE_URL が読み込めていません")
-
-    return psycopg.connect(database_url)
+    return psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", "5432"),
+        sslmode="require"
+    )
